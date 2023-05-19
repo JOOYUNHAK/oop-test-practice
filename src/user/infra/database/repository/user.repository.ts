d0df2d7157bc702e3d7bdefba1b0ad8678c6a6ac
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-19 17:10:28 +0900

    add(user): user repository and entity

---
+++ src/user/infra/database/repository/user.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "src/user/domain/entity/user-entity";
+import { IUserRepository } from "src/user/domain/repository/iuser.repository";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserRepository extends Repository<UserEntity> implements IUserRepository {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {
+        super(UserEntity, userRepository.manager)
+    }
+
+    async findByEmail(email: string): Promise<UserEntity> {
+        return await this.userRepository
+            .createQueryBuilder()
+            .where('email = :email', { email })
+            .getOne();
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,19 @@ import { Module } from "@nestjs/common";
 import { UserMapper } from "./application/mapper/user.mapper";
 import { UserService } from "./application/service/user.service";
 import { UserController } from "./interface/controller/user.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "./domain/entity/user-entity";
+import { UserRepository } from "./infra/database/repository/user.repository";
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([UserEntity]),
+    ],
     controllers: [UserController],
     providers: [
         UserMapper,
         UserService,
+        UserRepository
     ]
 }) 
 export class UserModule {};
