913fcbdac549553ac09cfb7c627e6f046f0498e7
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-20 19:43:14 +0900

    move(config): config module option folder
    mysql모듈에 잘못 있었던 위치 수정

--- src/config/config-module.option.ts
+++ src/config/config-module.option.ts
@@ -1,7 +1,8 @@
-import databaseConfiguration from "../../../../../../config/database.configuration";
+import databaseConfiguration from "../../config/database.configuration";
 import { ConfigModuleOptions } from "@nestjs/config";
 
 export const configModuleOption: ConfigModuleOptions = {
     isGlobal: true,
+    envFilePath: 'config/env/.development.env',
     load: [ databaseConfiguration]
 }
