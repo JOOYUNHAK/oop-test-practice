977eb840b32f6e74537170188b7f86fc370ee825
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-20 19:36:46 +0900

    add(auth): auth entity to mysql, user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,15 @@ import { UserMapper } from "./application/mapper/user.mapper";
 import { UserService } from "./application/service/user.service";
 import { UserController } from "./interface/controller/user.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntity } from "./domain/entity/user-entity";
+import { UserEntity } from "./domain/entity/user.entity";
 import { UserRepository } from "./infra/database/repository/user.repository";
+import { AuthModule } from "src/auth/auth.module";
+import { Authentication } from "src/auth/domain/authentication/entity/auth.entity";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([UserEntity]),
+        TypeOrmModule.forFeature([UserEntity, Authentication]),
+        AuthModule
     ],
     controllers: [UserController],
     providers: [
