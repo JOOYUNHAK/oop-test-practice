96e96f26d6ad66be31257175fe20db14d7d84c92
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-20 19:59:56 +0900

    modify(user): file name user-entity -> user.entity

--- src/user/domain/repository/iuser.repository.ts
+++ src/user/domain/repository/iuser.repository.ts
@@ -1,4 +1,4 @@
-import { UserEntity } from "../entity/user-entity";
+import { UserEntity } from "../entity/user.entity";
 
 export interface IUserRepository {
     findByEmail(email: string): Promise<UserEntity>
