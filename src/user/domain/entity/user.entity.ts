b48bf92a4909d93441e8d25f5ff0a86a3877c6c3
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-20 20:13:10 +0900

    add(user): user entity relation with auth

---
+++ src/user/domain/entity/user.entity.ts
@@ -0,0 +1,51 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { USER_STATUS } from '../enum/user-status.enum'
+import { Authentication } from "src/auth/domain/authentication/entity/auth.entity";
+
+@Entity('user')
+export class UserEntity {
+    @PrimaryGeneratedColumn('increment')
+    private readonly id: number;
+
+    @Column({ type: 'varchar', length: 50 })
+    private readonly email: string;
+
+    @Column({ type: 'varchar',length: 72 })
+    private readonly password: string;
+
+    @Column({ type: 'timestamp', name: 'registered_at' })
+    private readonly registeredAt: Date;
+
+    @Column({ type: 'timestamp', name: 'updated_at' })
+    private readonly updatedAt: Date; 
+
+    @Column({ type: 'tinyint', name: 'login_try' })
+    private readonly loginTry: number;
+
+    @Column({ type: 'timestamp', name: 'blocked_up_to', nullable: true })
+    private blockedUpTo: Date;
+
+    @Column({ type: 'enum', enum: USER_STATUS})
+    private readonly status: USER_STATUS;
+
+    @OneToOne(() => Authentication, { cascade: ['insert', 'remove'] })
+    @JoinColumn({name: 'authentication_id'})
+    private readonly authentication: Authentication
+    
+    constructor(
+        id: number, email: string, password: string, updatedAt: Date, registeredAt: Date, 
+        loginTry: number, blockedUpTo: Date, status: USER_STATUS, authentication: Authentication
+    ) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.registeredAt = registeredAt;
+        this.updatedAt = updatedAt;
+        this.loginTry = loginTry;
+        this.blockedUpTo = blockedUpTo;
+        this.status = status;
+        this.authentication = authentication;
+    }
+
+    getId(): number { return this.id; };
+}
