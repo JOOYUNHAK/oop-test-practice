9b3a93324c1cada87fa617e04a47fb5fa6ad3a8b
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-21 21:09:16 +0900

    modify(app): delete auth module to import in user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,11 @@ import { ConfigModule } from '@nestjs/config';
 import { configModuleOption } from './config/config-module.option';
 import { UserModule } from './user/user.module';
 import { MysqlModule} from './user/infra/database/mysql/mysql.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOption),
     MysqlModule,
-    AuthModule,
     UserModule 
   ],
   controllers: [AppController],
