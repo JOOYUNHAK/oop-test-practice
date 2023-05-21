f27aab8b58a77f82b3e26724838bd7f3bdafce5a
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-21 14:27:37 +0900

    add(auth): authentication interface method
    갱신을 위해 인증 인터페이스 업데이트,
    구현한 jwt token도 해당 메소드 구현

--- src/auth/domain/authentication/iauthentication.ts
+++ src/auth/domain/authentication/iauthentication.ts
@@ -1,5 +1,10 @@
 export interface IAuthentication {
     getClientAuthentication(): string;
     getRefreshAuthentication(): string;
-    getRefreshedTime(): Date
+    getRefreshedTime(): Date; // RefreshToken이 마지막으로 갱신된 날짜가 언제인지
+
+    updateClientAuthentication(clientAuthentication: string): void; 
+    updateRefreshAuthentication(refreshAuthentication: string): void;
+    
+    isNearingExpirationTime(): boolean; // 갱신할 날짜가 다가왔는지
 }
