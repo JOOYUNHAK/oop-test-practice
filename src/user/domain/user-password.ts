46b7e597ca02c0aa47b311a15da04fc8a2078281
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-24 20:12:02 +0900

    add(user): password object to entity

--- src/user/domain/user-password.ts
+++ src/user/domain/user-password.ts
@@ -1,25 +1,56 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user';
 
+@Entity('user_password')
 export class Password {
-    private readonly password: string;
-    private readonly isHashed: boolean;
+    @PrimaryGeneratedColumn('increment')
+    private id: number;
 
-    constructor( 
-        passowrd: string
-    ) { 
-        this.password = passowrd;
-        this.isHashed = true;
-     };
+    @Column({ type: 'varchar' })
+    private value: string;
 
-    getValue(): string { return this.password; };
+    @Column({ type: 'timestamp', name: 'updated_at' })
+    private updatedAt: Date
+
+    @OneToOne(() => User, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+    readonly user: User
+
+    constructor(
+        passowrd: string,
+        updatedAt: Date = new Date()
+    ) {
+        this.value = passowrd;
+        this.updatedAt = updatedAt;
+    };
+
+    getValue(): string { return this.value; };
+
+    /*  
+       암호화 되어 있는 비밀번호면 그대로 Return
+       암호화 되어 있지 않으면 암호화 해서 Return
+   */
+    static async create(password: string): Promise<Password> {
+        this.validate(password);
+        return new Password(await this.encrypt(password));
+    }
+
+    /* 비밀번호 변경 */
+    public async changePassword(old: string, toChange: string, updatedAt: Date): Promise<boolean> {
+        if (!await this.comparePassword(old)) return false;
+        if (this.equals(old, toChange))
+            throw new ConflictException('Duplicated Password');
+        this.changeSucceeded(toChange, updatedAt);
+        return true
+    }
 
     /* 비밀번호 유효성 검사 */
-    private static validate(plainTextPassword: string): boolean {
+    private static validate(plainTextPassword: string) {
         const passwordCheckReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$/;
-        if( !passwordCheckReg.test(plainTextPassword) )
+        if (!passwordCheckReg.test(plainTextPassword))
             throw new BadRequestException('Password Validate Failed');
-        return true;
     }
 
     /* 비밀번호 암호화 */
@@ -28,20 +59,16 @@ export class Password {
     }
 
     /* 암호화 되지 않은 비밀번호와 비교 */
-    public async comparePassword(
-        plainTextPassword: string, encryptedPassword: string
-    ): Promise<boolean> {
-        return await bcrypt.compare(plainTextPassword, encryptedPassword)
+    public async comparePassword(plainTextPassword: string): Promise<boolean> {
+        return await bcrypt.compare(plainTextPassword, this.value)
     }
 
-    /*  
-        암호화 되어 있는 비밀번호면 그대로 Return
-        암호화 되어 있지 않으면 암호화 해서 Return
-    */
-    static async create(password: string, isHashed: boolean): Promise<Password> { 
-        if( isHashed ) return new Password( password );
-        
-        this.validate(password);
-        return new Password( await this.encrypt(password) );
+    /* 비밀번호 변경 완료 */
+    private changeSucceeded(newPassword: string, updatedAt: Date) {
+        this.value = newPassword;
+        this.updatedAt = updatedAt;
     }
+
+    /* 기존 사용하던 비밀번호랑 같은지 */ 
+    private equals(old: string, toChange: string): boolean { return old === toChange; };
 }
