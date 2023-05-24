dcbd5335b873d4437e53edefd5d3d371068c7350
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-24 21:30:04 +0900

    add(gitignore): config file for testing local

--- config/database.configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-    MYSQL: {
-        HOST: 'database-1.cyhk2vuwnvlk.ap-northeast-2.rds.amazonaws.com',
-        PORT: 3306,
-        USERNAME: 'root',
-        PASSWORD: '12345678',
-        DATABASE: 'aaant'
-    }
-}) 
