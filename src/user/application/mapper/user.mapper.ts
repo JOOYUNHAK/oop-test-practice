2d1d84555e328f85dbb7c5d1e57922380df50c19
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-24 20:20:35 +0900

    modify(user): usermapper change by convert entity

--- src/user/application/mapper/user.mapper.ts
+++ src/user/application/mapper/user.mapper.ts
@@ -1,9 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { AuthService } from "src/auth/application/service/auth.service";
-import { UserAuthentication } from "src/auth/domain/authentication/authentication";
-import { Authentication } from "src/auth/domain/authentication/entity/auth.entity";
-import { JwtToken } from "src/auth/infra/authentication/token/jwt-token";
-import { UserEntity } from "src/user/domain/entity/user.entity";
+import { LoginBlockInfo } from "src/user/domain/login-block-info";
 import { User } from "src/user/domain/user";
 import { Email } from "src/user/domain/user-email";
 import { Password } from "src/user/domain/user-password";
@@ -20,7 +17,7 @@ export class UserMapper {
         const email = Email.create(registerDto.email); 
         /* 비밀번호 유효성 검사와 인증을 얻어 새로운 사용자로 변환 */
         const [encryptedPassword, authentication] = await Promise.all([
-            Password.create(registerDto.password, false),
+            Password.create(registerDto.password),
             this.authService.createAuthentication(registerDto.email)
         ]);
         return new User(
@@ -29,41 +26,9 @@ export class UserMapper {
             encryptedPassword,
             authentication,
             new Date(),
-            new Date(),
-            0,
-            null,
+            new LoginBlockInfo(0, null),
             null,
         );
-    }
-
-    /* 도메인 모델을 Entity로 */
-    domainToEntity(user: User): UserEntity {
-        return new UserEntity(
-            user.getId(),
-            user.getEmail().getValue(),
-            user.getPassword().getValue(),
-            user.getUpdateTime(), 
-            user.getRegisteredTime(),
-            user.getLoginTryCount(),
-            user.getBlockedTime(),
-            user.getStatus(),
-            this.authDomainToEntity(user.getAuthentication())
-        )
-    }
-
-    /* Entity를 도메인 모델로 */
-    async entityToDomain(userEntity: UserEntity): Promise<User> {
-        return new User(
-            userEntity.getId(),
-            Email.create(userEntity.getEmail()),
-            await Password.create(userEntity.getPassword(), true),
-            this.authEntityToDomain(userEntity.getId(), userEntity.getAuthentication()),
-            userEntity.getUpdatedTime(),
-            userEntity.getRegisteredTime(),
-            userEntity.getLoginTry(),
-            userEntity.getBlockedTime(),
-            userEntity.getStatus()
-        )
     }
 
     /* 도메인 모델을 사용자에게 전달할 Dto로 */
@@ -72,22 +37,5 @@ export class UserMapper {
             id: user.getId(),
             accessToken: user.getClientAuthentication()
         }
-    }
-
-    /* 새로 생성하여 받은 인증을 Auth 테이블에 저장하기 위해 Entity로 */
-    private authDomainToEntity(userAuthentication: UserAuthentication): Authentication {
-        return new Authentication(
-            userAuthentication.getId(),
-            userAuthentication.getType().getRefreshAuthentication(), // 인증 갱신용 -> DB 저장
-            userAuthentication.getType().getRefreshedTime() // 마지막으로 갱신된 시간
-        )
-    } 
-
-    /* UserEntity에 있는 인증을 도메인 모델로 변경 */
-    private authEntityToDomain(userId: number, authentication: Authentication): UserAuthentication {
-        return new UserAuthentication(
-            userId,  
-            new JwtToken(null, authentication.getRefreshAuthentication(), authentication.getUpdatedTime())
-        )
     }
 }
