de323d343b1e5843cae05c439916272437ac1e45
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-25 01:18:56 +0900

    add(common): exception error enum

---
+++ src/common/exception/enum/error-message.enum.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessage {
+    PASSWORD_VALIDATE = 'Password Validate Failed',
+    LOGIN_FAILED = 'Something Error Id Or Password',
+    DUPLICATED_PASSWORD = 'Duplicated Password'
+}
