1109db3a76f8e6a73fc4d73767fabec4bff2657a
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-27 23:54:43 +0900

    add(common): common error message

--- src/common/exception/enum/error-message.enum.ts
+++ src/common/exception/enum/error-message.enum.ts
@@ -1,5 +1,7 @@
 export enum ErrorMessage {
+    EMAIL_VALIDATE = 'Email Validate Failed',
     PASSWORD_VALIDATE = 'Password Validate Failed',
     LOGIN_FAILED = 'Something Error Id Or Password',
-    DUPLICATED_PASSWORD = 'Duplicated Password'
+    DUPLICATED_PASSWORD = 'Duplicated Password',
+    DUPLICATED_EMAIL = 'Duplicated Email',
 }
