8cf3a666cc7cb42885e44f7078871de5508cf369
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-28 23:49:46 +0900

    add(common): login count exceed error message

--- src/common/exception/enum/error-message.enum.ts
+++ src/common/exception/enum/error-message.enum.ts
@@ -4,4 +4,5 @@ export enum ErrorMessage {
     LOGIN_FAILED = 'Something Error Id Or Password',
     DUPLICATED_PASSWORD = 'Duplicated Password',
     DUPLICATED_EMAIL = 'Duplicated Email',
+    LOGIN_COUNT_EXCEED = 'Login Try Count Exceed'
 }
