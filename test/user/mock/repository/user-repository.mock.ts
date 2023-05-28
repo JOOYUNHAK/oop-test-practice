e6ea4f2fd4e5b013253be29d833d080924a3cbcc
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-28 23:55:22 +0900

    test(user): user repository mocking

--- test/user/mock/repository/user-repository.mock.ts
+++ test/user/mock/repository/user-repository.mock.ts
@@ -1,6 +1,7 @@
-import { UserStub } from "test/user/stub/user.stub";
+import { User } from "src/user/domain/user";
 
-export const UserRepositoryMock =  {
-    save: jest.fn(() => UserStub()),
-    findByEmail: jest.fn()
-}
+export const UserRepositoryMock = () => ({
+    save: jest.fn((user):User => user),
+    findByEmail: jest.fn(),
+    delete: jest.fn(),
+});
