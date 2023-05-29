58f8e89421f04bc15282e06afdd7de7922ac8761
JOOYUNHAK <wndbsgkr@naver.com>
2023-05-29 21:19:51 +0900

    add(common): error message enum

--- src/common/exception/enum/error-message.enum.ts
+++ src/common/exception/enum/error-message.enum.ts
@@ -1,8 +1,11 @@
 export enum ErrorMessage {
     EMAIL_VALIDATE = 'Email Validate Failed',
     PASSWORD_VALIDATE = 'Password Validate Failed',
-    LOGIN_FAILED = 'Something Error Id Or Password',
     DUPLICATED_PASSWORD = 'Duplicated Password',
     DUPLICATED_EMAIL = 'Duplicated Email',
-    LOGIN_COUNT_EXCEED = 'Login Try Count Exceed'
+    LOGIN_FAILED = 'Something Error Id Or Password',
+    LOGIN_COUNT_EXCEED = 'Login Try Count Exceed',
+    NO_EXIST_USER_IN_AUTHENTICATION = 'Not Found User In Authentication',
+    AUTHENTICATION_EXPIRED = 'Login Again Please',
+    NO_AUTHENTICATION_IN_HEADER = 'No Authentication In Header'
 }
