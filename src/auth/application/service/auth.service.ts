dc0625e31e4992b8c06796c2342b92be00def80d
JOOYUNHAK <wndbsgkr@naver.com>
2023-11-09 23:39:03 +0900

    add: 깃허브 태그 테스트용 커밋

--- src/auth/application/service/auth.service.ts
+++ src/auth/application/service/auth.service.ts
@@ -28,6 +28,7 @@ export class AuthService {
             await this.updateUserAuthentication(user);
         /* 로그인에 성공했으므로 마지막 갱신일, 실패했으면 실패 로직에 따른 결과 업데이트 */
         await this.userAuthCommonRepository.save(user);
+        console.log('GITHUB')
         user.statusCheck(); // 비밀번호를 틀렸을 경우 Throw Error
         return this.userAuthCommonMapper.toDto(user);
     }
